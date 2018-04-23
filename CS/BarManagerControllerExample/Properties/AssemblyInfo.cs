8f6b5e00a38318e700ac7df069499bde8119262b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BarManagerControllerExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,68 @@
+﻿// Developer Express Code Central Example:
+// How to define bars and bar items via a template
+// 
+// This example shows how to define a bar, bar items and bar item links in a
+// DataTemplate, and then use this template to create these objects in a
+// BarManager. Bar objects are defined via the DataTemplate class. The
+// DataTemplate's content must be a BarManagerActionContainer object, containing
+// actions to create bar objects. The created template is used within the
+// BarManager via the BarManager.Controllers collection.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E1731
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BarManagerControllerExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarManagerControllerExample")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to define bars and bar items via a template
+
+
+<p>This example demonstrates how to modify the current BarManager's layout in case you do not have direct access to this BarManager. The main idea is to use ControllerBehavior that provides the capability to compose bars or ribbons from templates using built-in <a href="https://documentation.devexpress.com/WPF/7045/Controls-and-Libraries/Ribbon-Bars-and-Menu/Bars/Bar-Actions">Bar Actions</a>.<br><br><em>For versions prior to 15.1:</em><br>Use BarManager.Controllers to set a predefined DataTemplate.</p>
+
+<br/>
+
+
